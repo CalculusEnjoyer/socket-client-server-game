9acae709cf9b8586f0f57d0c80346ae99b4777d2
CalculusEnjoyer <kravchukzxy@gmail.com>
2022-11-05 13:28:57 +0200

    First working vertion

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import Client.Client;
+import Server.Server;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    Client client = new Client();
+    client.startConnection(Server.IP, Server.PORT);
+
+    System.out.println(client.sendMessage("check"));
+
+    Scanner sc = new Scanner(System.in);
+    while(true){
+      System.out.println(client.sendMessage(sc.nextLine()));
+    }
+  }
+}
